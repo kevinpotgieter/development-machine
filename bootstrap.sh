4115af267c2aea59a78094d78362c0787b4bbd05
Kevin Potgieter <kevin.potgieter@bluedawnsolutions.co.uk>
2016-05-31 12:03:37 +0100

    Adding in Shiftit and GoogleCloud sdk to the list of installable items.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,8 @@
-sudo easy_install pip
+echo 'installing latest version of pip using easy_install'
+sudo easy_install --upgrade pip
+
+# echo 'upgrading pip if there is a newer one available'
+# sudo pip install --upgrade pip
+
+echo 'installing ansible via pip'
 sudo pip install ansible
